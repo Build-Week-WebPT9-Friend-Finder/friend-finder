bf275712844721ba51725016b2260a683a277233
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-10-17 20:12:15 -0700

    Added in axiosWithAuth

---
+++ friend-finder/src/components/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const auth_token = localStorage.getItem('auth_token');
+
+  return axios.create({
+    baseURL: 'https://friend-finder-levi.herokuapp.com/api',
+    headers: {
+      "Authorization": auth_token
+    }
+  });
+};
