82299f7d468b01af50b870b4eeac4c616c5e6f15
Jason Barr <jason@jasonsbarr.com>
2019-10-23 18:34:40 -0500

    Reformat with Prettier

--- src/component/Router/PrivateRouter.js
+++ src/component/Router/PrivateRouter.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const PrivateRouter = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props => {
-        if (localStorage.getItem('access_token')) {
+        if (localStorage.getItem("access_token")) {
           return <Component {...props} />;
         } else {
           return <Redirect to="login" />;
