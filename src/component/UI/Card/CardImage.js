c7d82588e50e2a65ddd84ab23bc3b676ca2e9de3
Jason Barr <jason@jasonsbarr.com>
2019-10-23 02:43:22 -0500

    Create and style Card Image with container

--- src/component/UI/Card/CardImage.js
+++ src/component/UI/Card/CardImage.js
@@ -2,8 +2,21 @@
 import React from "react";
 import { css, jsx } from "@emotion/core";
 
-const CardImage = ({ img, alt = "" }) => (
-  <div>
+const styles = css`
+  background: rgba(0, 0, 0, 0.05);
+  display: block;
+  flex: 0 0 auto;
+  padding: 0;
+  position: relative;
+`;
+
+const CardImage = ({ img, alt = "", className = "" }) => (
+  <div
+    css={styles}
+    className={`${
+      className ? className + " card-image" : "card-image"
+    }`}
+  >
     <img src={img} alt={alt} />
   </div>
 );
