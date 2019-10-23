7f095115b8687ee822c959fa4356295b711e04ad
Jason Barr <jason@jasonsbarr.com>
2019-10-23 02:42:54 -0500

    Create and style Card Header to take any element type

--- src/component/UI/Card/CardHeader.js
+++ src/component/UI/Card/CardHeader.js
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-const CardHeader = props => {
-  const { elem, children, ...rest } = props;
-};
+const Header = ({ Elem, children, className = "", ...props }) => (
+  <Elem
+    {...props}
+    className={`${
+      className ? className + " card-header" : "card-header"
+    }`}
+  >
+    {children}
+  </Elem>
+);
+
+const CardHeader = styled(Header)`
+  color: rgba(0, 0, 0, 0.85);
+  display: block;
+  font-size: 1.25rem;
+  font-weight: bold;
+  line-height: 1.25rem;
+  margin-block-end: 0;
+  margin-block-start: 0;
+  margin-top: -0.25rem;
+
+  a {
+    background-color: transparent;
+    text-decoration: none;
+  }
+`;
 
 export default CardHeader;
