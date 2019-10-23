4748c7cd40989566e5ac7d7da931a2061fd8ed42
Jason Barr <jason@jasonsbarr.com>
2019-10-23 13:11:12 -0500

    Add WebFontLoader for Google Fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import WebFont from "webfontloader";
 import App from "./App";
+
+WebFont.load({
+  google: {
+    families: [
+      "Open Sans:400,400i,700,700i",
+      "Arimo:400,400i,700,700i",
+      "Lato:400,400i,700,700",
+    ],
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
