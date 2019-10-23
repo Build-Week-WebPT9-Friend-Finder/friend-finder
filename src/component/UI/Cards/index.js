996034db8a2e2d75e5ec74ba408f1d5965330871
Jason Barr <jason@jasonsbarr.com>
2019-10-23 02:45:08 -0500

    Create and style Cards for a row/collection of Card components

--- src/component/UI/Cards/index.js
+++ src/component/UI/Cards/index.js
@@ -1,5 +1,48 @@
-import React from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-const Cards = props => <div>{props.children}</div>;
+const styles = css`
+  display: flex;
+  flex-wrap: wrap;
+  margin: -0.875rem -0.5rem;
+
+  &::after {
+    clear: both;
+    content: " ";
+    display: block;
+    height: 0;
+    overflow: hidden;
+    visibility: hidden;
+  }
+
+  > .card {
+    border: none;
+    border-radiux: 4px;
+    box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
+    display: flex;
+    flex-direction: column;
+    float: none;
+    font-size: 1rem;
+    margin: 0.875rem 0.5rem;
+    max-width: 100%;
+    min-height: 0;
+    padding: 0;
+    position: relative;
+    transition: box-shadow .1s ease,transform .1s
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    width: 290px;
+  }
+`;
+
+const Cards = ({ children, className = "", ...props }) => (
+  <div
+    {...props}
+    css={styles}
+    className={`${className ? className + " cards" : "cards"}`}
+  >
+    {children}
+  </div>
+);
 
 export default Cards;
