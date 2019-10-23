581831611442ac50251ccec51c78f57b49207fb9
Jason Barr <jason@jasonsbarr.com>
2019-10-23 02:42:18 -0500

    Create and style Card Description

--- src/component/UI/Card/CardDescription.js
+++ src/component/UI/Card/CardDescription.js
@@ -1,7 +1,22 @@
-import React from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-const CardDescription = ({ text }) => (
-  <div className="description">{text}</div>
+const styles = css`
+  clear: both;
+  color: rgba(0, 0, 0, 0.68);
+  margin-top: 0.5rem;
+`;
+
+const CardDescription = ({ children, className = "", ...props }) => (
+  <div
+    css={styles}
+    {...props}
+    className={`${
+      className ? className + " card-description" : "card-description"
+    }`}
+  >
+    {children}
+  </div>
 );
 
 export default CardDescription;
