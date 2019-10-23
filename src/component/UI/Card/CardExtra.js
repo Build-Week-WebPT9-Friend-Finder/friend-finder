120bcb01c8aceeb49e5f43dbfea07d1964ea3a16
Jason Barr <jason@jasonsbarr.com>
2019-10-23 02:44:16 -0500

    Create and style Card Extra-
    Content

--- src/component/UI/Card/CardExtra.js
+++ src/component/UI/Card/CardExtra.js
@@ -1,5 +1,43 @@
-import React from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-const CardExtra = props => <div>{props.children}</div>;
+const styles = css`
+  background: 0 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.05) !important;
+  box-shadow: none;
+  color: rgba(0, 0, 0, 0.4);
+  flex-grow: 0;
+  left: 0;
+  margin: 0 0;
+  max-width: 100%;
+  min-height: 0 !important;
+  padding: 0.75rem 1rem;
+  position: static;
+  top: 0;
+  transition: color 0.1s ease;
+  -webkit-box-flex: 0;
+  width: auto;
+
+  &::after {
+    clear: both;
+    content: " ";
+    display: block;
+    height: 0;
+    overflow: hidden;
+    visibility: hidden;
+  }
+`;
+
+const CardExtra = ({ children, className = "", ...props }) => (
+  <div
+    {...props}
+    css={styles}
+    className={`${
+      className ? className + " card-content" : "card-content"
+    }`}
+  >
+    {children}
+  </div>
+);
 
 export default CardExtra;
