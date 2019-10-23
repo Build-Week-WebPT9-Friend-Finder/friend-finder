013fc8b11f3064c81691fa461f8bf0d1dfd1f1b8
Jason Barr <jason@jasonsbarr.com>
2019-10-23 02:41:21 -0500

    Create and style avatar-style image

--- src/component/UI/Card/CardAvatar.js
+++ src/component/UI/Card/CardAvatar.js
@@ -1,5 +1,24 @@
-import React from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-const CardAvatar = ({ src, alt = "" }) => <img src={src} alt={alt} />;
+const styles = css`
+  border-radius: 500rem;
+  display: inline-block;
+  height: 2rem;
+  margin-right: 0.25rem;
+  width: 2rem;
+`;
+
+const CardAvatar = ({ img, alt = "", className = "", ...props }) => (
+  <img
+    {...props}
+    css={styles}
+    className={`${
+      className ? className + " card-avatar" : "card-avatar"
+    }`}
+    src={img}
+    alt={alt}
+  />
+);
 
 export default CardAvatar;
