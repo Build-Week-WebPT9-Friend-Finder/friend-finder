64b84adae3569463b31b0ceec62e5999de6e7769
Jason Barr <jason@jasonsbarr.com>
2019-10-23 21:48:38 -0500

    Render children in squeeze page

--- src/layouts/templates/SqueezePage.js
+++ src/layouts/templates/SqueezePage.js
@@ -7,14 +7,16 @@ const styles = css`
   height: 100%;
   min-height: 100%;
   min-width: 100%;
+  overflow-x: hidden;
   width: 100%;
 `;
 
-const SqueezePage = props => (
+const SqueezePage = ({ children }) => (
   <div className="app-wrapper" css={styles}>
     <AppHeader>
       <Logo />
     </AppHeader>
+    {children}
   </div>
 );
 
