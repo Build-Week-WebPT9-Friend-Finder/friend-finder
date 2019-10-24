bdc4434e8e08afd85b35e7c93f6ab53f98a87bdd
Jason Barr <jason@jasonsbarr.com>
2019-10-24 13:28:50 -0500

    Initial Redux setup

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import { axiosWithAuth } from "../component/axiosWithAuth";
+
+export const FETCHING_USERS_START = "FETCHING_USERS_START";
+export const FETCHING_USERS_SUCCESS = "FETCHING_USERS_SUCCESS";
+export const FETCHING_USERS_FAILURE = "FETCHING_USERS_FAILURE";
+
+export const getUser = () => dispatch => {
+  dispatch({ type: FETCHING_USERS_START });
+  axiosWithAuth()
+    .get("/user")
+    .then(res => {
+      console.log(res.data);
+      dispatch({ type: FETCHING_USERS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCHING_USERS_FAILURE, payload: err.data });
+    });
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+import {
+  FETCHING_USERS_START,
+  FETCHING_USERS_SUCCESS,
+  FETCHING_USERS_FAILURE,
+} from "../actions";
+
+const initialState = {
+  users: [],
+  isFetching: false,
+  error: "",
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_USERS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: "",
+      };
+    case FETCHING_USERS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        users: action.payload,
+      };
+    case FETCHING_USERS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false,
+      };
+    default:
+      return state;
+  }
+};
