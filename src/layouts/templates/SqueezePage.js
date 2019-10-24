0d0c911d3cb5ca33c43e82fe938d2ad2c2ea3a4e
Jason Barr <jason@jasonsbarr.com>
2019-10-23 21:47:16 -0500

    Set minimum height to viewport height

--- src/layouts/templates/SqueezePage.js
+++ src/layouts/templates/SqueezePage.js
@@ -1,15 +1,21 @@
 /** @jsx jsx */
-import React from "react";
 import { css, jsx } from "@emotion/core";
 import AppHeader from "./partials/AppHeader";
 import Logo from "../../component/Logo";
 
+const styles = css`
+  height: 100%;
+  min-height: 100%;
+  min-width: 100%;
+  width: 100%;
+`;
+
 const SqueezePage = props => (
-  <>
+  <div className="app-wrapper" css={styles}>
     <AppHeader>
       <Logo />
     </AppHeader>
-  </>
+  </div>
 );
 
 export default SqueezePage;
