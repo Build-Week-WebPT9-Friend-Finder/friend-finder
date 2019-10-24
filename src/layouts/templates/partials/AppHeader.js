ac6f11547fa52a397d8071d91575ad620961aab5
Jason Barr <jason@jasonsbarr.com>
2019-10-23 22:38:51 -0500

    Display flex to properly arrange Header children
    Also fix import link for colors

--- src/layouts/templates/partials/AppHeader.js
+++ src/layouts/templates/partials/AppHeader.js
@@ -1,11 +1,14 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { primaryLight, greyLight } from "../../sass/_colors.scss";
+import { primaryLight, greyLight } from "../../../sass/_colors.scss";
 
 const styles = css`
+  align-items: center;
   background-color: ${primaryLight};
-  box-shadow: 2px 1px 2px ${greyLight}
+  box-shadow: 2px 1px 2px ${greyLight};
+  display: flex;
   height: 4rem;
+  justify-content: flex-start;
 `;
 
 const AppHeader = ({ children }) => (
