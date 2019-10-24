5f71857ddae45b7f9c7186da2a89067153748f60
Jason Barr <jason@jasonsbarr.com>
2019-10-24 13:27:59 -0500

    Revert "ga"
    This reverts commit ea6669c3277cf50494ee55521011c208143aafed.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import WebFont from "webfontloader";
-import { reducer } from "./reducers";
+import App from "./App";
 
 WebFont.load({
   google: {
@@ -17,17 +13,4 @@ WebFont.load({
   },
 });
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(thunk)),
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />,
-  </Provider>,
-  document.getElementById("root"),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
