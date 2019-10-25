a98b6859185380546246f92743d287880f2acfb7
Jason Barr <jason@jasonsbarr.com>
2019-10-25 02:32:32 -0500

    Remove Switch and PrivateRouter, redirect Unauthenticated to Login

--- src/component/AuthenticatedApp.js
+++ src/component/AuthenticatedApp.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import AppPage from "../layouts/templates/AppPage";
 import Home from "./Home";
 import Logout from "./Logout";
-import PrivateRouter from "./Router/PrivateRouter";
 
 const AuthenticatedApp = () => (
   <AppPage auth={1}>
-    <Switch>
-      <PrivateRouter path="/" exact component={Home} />
-      <PrivateRouter path="/logout" exact component={Logout} />
-    </Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/logout" exact component={Logout} />
   </AppPage>
 );
 
