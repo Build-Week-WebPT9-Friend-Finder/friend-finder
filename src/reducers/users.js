89ee2d9c0a76dc4c127aa56bf7d148277194f126
Jason Barr <jason@jasonsbarr.com>
2019-10-24 21:05:09 -0500

    Rename reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -10,7 +10,7 @@ const initialState = {
   error: "",
 };
 
-export const usersReducer = (state = initialState, action) => {
+export const users = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING_USERS_START:
       return {
