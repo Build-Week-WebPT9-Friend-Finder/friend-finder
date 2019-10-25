ef2ea153c7ce44e5f4b2dbcbe713df5af33dc544
Jason Barr <jason@jasonsbarr.com>
2019-10-24 23:09:48 -0500

    Refactor survey into its own component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import WelcomeToSurvey from "./components/Surveycard1";
-import SurveyQuestionnaire from "./components/Questionnaire";
 import ReactRouter from "./component/Router/Router";
 import "./sass/index.scss";
 import SqueezePage from "./layouts/templates/SqueezePage";
@@ -8,13 +6,6 @@ import SqueezePage from "./layouts/templates/SqueezePage";
 function App() {
   return (
     <div className="App">
-      <div className="Container">
-        <WelcomeToSurvey />
-      </div>
-      <div>
-        <SurveyQuestionnaire />
-      </div>
-
       <SqueezePage>
         <ReactRouter />
       </SqueezePage>
